3320886a605849a67df8d6eb9717cf6908e29091
Thang Vuong <thangvuongbk@gmail.com>
2016-10-25 00:53:55 +0700

    commit tomato project

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TomatoTimer
+{
+    partial class TomatoTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TomatoTimer));
+            this.btnPomodoro = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnShorBreak = new System.Windows.Forms.Button();
+            this.btnLongBreak = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lbMinute = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbSecond = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPomodoro
+            // 
+            this.btnPomodoro.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnPomodoro.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPomodoro.Location = new System.Drawing.Point(1, 0);
+            this.btnPomodoro.Name = "btnPomodoro";
+            this.btnPomodoro.Size = new System.Drawing.Size(94, 29);
+            this.btnPomodoro.TabIndex = 3;
+            this.btnPomodoro.Text = "Pomodoro";
+            this.btnPomodoro.UseVisualStyleBackColor = false;
+            this.btnPomodoro.Click += new System.EventHandler(this.btnPomodoro_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnShorBreak
+            // 
+            this.btnShorBreak.BackColor = System.Drawing.Color.SkyBlue;
+            this.btnShorBreak.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShorBreak.Location = new System.Drawing.Point(93, 0);
+            this.btnShorBreak.Name = "btnShorBreak";
+            this.btnShorBreak.Size = new System.Drawing.Size(98, 29);
+            this.btnShorBreak.TabIndex = 4;
+            this.btnShorBreak.Text = "Short Break";
+            this.btnShorBreak.UseVisualStyleBackColor = false;
+            this.btnShorBreak.Click += new System.EventHandler(this.btnShorBreak_Click);
+            // 
+            // btnLongBreak
+            // 
+            this.btnLongBreak.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnLongBreak.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLongBreak.Location = new System.Drawing.Point(190, 0);
+            this.btnLongBreak.Name = "btnLongBreak";
+            this.btnLongBreak.Size = new System.Drawing.Size(94, 29);
+            this.btnLongBreak.TabIndex = 5;
+            this.btnLongBreak.Text = "Long Break";
+            this.btnLongBreak.UseVisualStyleBackColor = false;
+            this.btnLongBreak.Click += new System.EventHandler(this.btnLongBreak_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(1, 136);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(94, 27);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Crimson;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(100, 136);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(91, 27);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft New Tai Lue", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(194, 136);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(90, 27);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lbMinute
+            // 
+            this.lbMinute.AutoSize = true;
+            this.lbMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMinute.Location = new System.Drawing.Point(-17, 30);
+            this.lbMinute.Name = "lbMinute";
+            this.lbMinute.Size = new System.Drawing.Size(151, 107);
+            this.lbMinute.TabIndex = 9;
+            this.lbMinute.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(119, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 76);
+            this.label3.TabIndex = 10;
+            this.label3.Text = ":";
+            // 
+            // lbSecond
+            // 
+            this.lbSecond.AutoSize = true;
+            this.lbSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 70F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSecond.Location = new System.Drawing.Point(151, 30);
+            this.lbSecond.Name = "lbSecond";
+            this.lbSecond.Size = new System.Drawing.Size(151, 107);
+            this.lbSecond.TabIndex = 11;
+            this.lbSecond.Text = "00";
+            // 
+            // TomatoTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(287, 167);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lbSecond);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPomodoro);
+            this.Controls.Add(this.lbMinute);
+            this.Controls.Add(this.btnShorBreak);
+            this.Controls.Add(this.btnLongBreak);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TomatoTimer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Tomato Timer";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPomodoro;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnShorBreak;
+        private System.Windows.Forms.Button btnLongBreak;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lbMinute;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbSecond;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TomatoTimer
+{
+    public partial class TomatoTimer : Form
+    {
+        ///
+        int iMinute = 0;
+        int iSecond = 0;
+        bool fPodomoro = false;
+        bool fShort = false;
+        bool fLong = false;        
+     //   string path = Directory.GetCurrentDirectory();
+        SoundPlayer simpleSound = new SoundPlayer(Directory.GetCurrentDirectory() + @"\alarmAlert.wav");
+
+
+        public TomatoTimer()
+        {
+            InitializeComponent();
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width,
+                                      workingArea.Bottom - Size.Height);
+        }
+        private void playSimpleSound()
+        {
+             simpleSound.Play();           
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            iMinute = 25;
+            iSecond = 00;
+            lbMinute.Text = iMinute.ToString();
+            lbSecond.Text = "0" + iSecond.ToString();            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            //
+            iSecond--;
+            if(iSecond <= 0)
+            {
+                iSecond = 59;
+                iMinute--;
+                if (iMinute < 0)
+                {
+                    iMinute = 0;
+                    iSecond = 0;
+                    timer1.Stop();
+                    playSimpleSound();
+                }
+            }
+            //
+            if (iSecond < 10 && iMinute < 10)
+            {
+                lbSecond.Text = "0" + iSecond.ToString();
+                lbMinute.Text = "0" + iMinute.ToString();
+            }
+            else if (iSecond < 10 && iMinute > 10)
+            {
+                lbSecond.Text = "0" + iSecond.ToString();
+                lbMinute.Text = iMinute.ToString();
+            }
+            else if (iMinute < 10)
+            {
+                lbMinute.Text = "0" + iMinute.ToString();
+                lbSecond.Text = iSecond.ToString();                
+            }           
+            else
+            {
+                lbMinute.Text = iMinute.ToString();
+                lbSecond.Text = iSecond.ToString();
+            }
+            
+        }
+
+        private void btnPomodoro_Click(object sender, EventArgs e)
+        {
+            iMinute = 25;
+            iSecond = 00;
+            lbMinute.Text = iMinute.ToString();
+            lbSecond.Text = "0" + iSecond.ToString();
+            // 
+            fPodomoro = true;
+            fShort = false;
+            fLong = false;
+            simpleSound.Stop();
+            timer1.Start();
+            //playSimpleSound();
+        }
+
+        private void btnShorBreak_Click(object sender, EventArgs e)
+        {
+            iMinute = 05;
+            iSecond = 00;
+            lbMinute.Text = "0" + iMinute.ToString();
+            lbSecond.Text = "0" + iSecond.ToString();
+            // 
+            fPodomoro = false;
+            fShort = true;
+            fLong = false;
+            simpleSound.Stop();
+            timer1.Start();
+        }
+
+        private void btnLongBreak_Click(object sender, EventArgs e)
+        {
+            iMinute = 10;
+            iSecond = 00;
+            lbMinute.Text = iMinute.ToString();
+            lbSecond.Text = "0" + iSecond.ToString();
+            // 
+            fPodomoro = false;
+            fShort = false;
+            fLong = true;
+            simpleSound.Stop();
+            timer1.Start();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            simpleSound.Stop();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            simpleSound.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            simpleSound.Stop();
+            if (fPodomoro == true)
+            {
+                iMinute = 25;
+                iSecond = 00;
+                lbMinute.Text = iMinute.ToString();
+                lbSecond.Text = "0" + iSecond.ToString();
+                timer1.Stop();
+            }
+            else if(fShort)
+            {
+                iMinute = 05;
+                iSecond = 00;
+                lbMinute.Text = "0" + iMinute.ToString();
+                lbSecond.Text = "0" + iSecond.ToString();
+                timer1.Stop();
+            }
+            else if(fLong)
+            {
+                iMinute = 10;
+                iSecond = 00;
+                lbMinute.Text = iMinute.ToString();
+                lbSecond.Text = "0" + iSecond.ToString();
+                timer1.Stop();
+            }
+           
+        }
+    }
+}
